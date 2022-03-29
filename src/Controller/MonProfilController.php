e6b8550780bb43d0cc21d5c1df848b9928a6da98
naelalaji <na2el_alaje@hotmail.com>
2022-03-29 16:44:55 +0200

    nael

--- src/Controller/MonProfilController.php
+++ src/Controller/MonProfilController.php
@@ -2,20 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\ParticipantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MonProfilController extends AbstractController
 {
+
+    private $participantRepo ;
+
+    function __construct(ParticipantRepository $participantRepo)// injection de dépendances
+    {
+        $this->participantRepo = $participantRepo;
+    }
+
     /**
      * @Route("/mon/profil", name="app_mon_profil")
      */
-    public function index(): Response
+    public function monProfil(): Response
     {
 
-        return $this->render('mon_profil/index.html.twig', [
-            'controller_name' => 'MonProfilController',
-        ]);
+
+        $UserProfil = $this->participantRepo->findBy(["pseudo"=>true]);
+
+        return $this->render('mon_profil/index.html.twig',compact($UserProfil));
     }
 }
