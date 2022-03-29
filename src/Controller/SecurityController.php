478037050a49b770eee80ae0f58216931de0f1e8
naelalaji <na2el_alaje@hotmail.com>
2022-03-29 14:40:01 +0200

    inscription ok se souvenir de moi ok

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -29,10 +29,10 @@ class SecurityController extends AbstractController
     /**
      * @Route("/logout", name="app_logout")
      */
-    public function logout():Response
+    public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-        return $this->redirectToRoute('app_login');
+
     }
 
 }
