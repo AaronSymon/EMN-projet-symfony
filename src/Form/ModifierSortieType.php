8d86b09c5cd913bf151c02b37a26e28ee4b9b304
AaronSymon <edouard.laine2@gmail.com>
2022-03-31 13:32:52 +0200

    modification d'une sortie en cours (pas ok) 2

---
+++ src/Form/ModifierSortieType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lieu;
+use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ModifierSortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('dateHeureDebut')
+            ->add('duree')
+            ->add('dateLimiteInscription')
+            ->add('nbInscriptionMax')
+            ->add('infosSortie')
+            ->add('SortieLieu', EntityType::class,[
+                'class'=>Lieu::class,
+                'choice_label'=>function(Lieu $lieu){
+                    return $lieu->getNom();
+                },
+                'mapped'=>false,
+                'multiple'=>false,
+                'expanded'=>true
+            ])
+            ->add('modifierSortie',SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
