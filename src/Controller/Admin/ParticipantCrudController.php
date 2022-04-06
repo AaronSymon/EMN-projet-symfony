955f2076e60e6ba309de5cf91c539668fb4553dd
Ledrert <marc.10.lefebvre@gmail.com>
2022-04-06 10:48:30 +0200

    Hashage password Admin

--- src/Controller/Admin/ParticipantCrudController.php
+++ src/Controller/Admin/ParticipantCrudController.php
@@ -5,6 +5,10 @@ namespace App\Controller\Admin;
 use App\Entity\Participant;
 use App\Entity\Site;
 use App\Form\RegistrationFormType;
+use App\Form\SiteType;
+use Doctrine\DBAL\Types\IntegerType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Id;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Menu\MenuItemTrait;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
@@ -21,7 +25,9 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 use EasyCorp\Bundle\EasyAdminBundle\Form\Type\FileUploadType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Encoder\PasswordHasherAdapter;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 
 class ParticipantCrudController extends AbstractCrudController
--- src/EventSubscriber/EasyAdminSubscriber.php
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -1,46 +1,95 @@
 <?php
+
 namespace App\EventSubscriber;
 
 use App\Entity\BlogPost;
+use App\Entity\Participant;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Validator\Constraints\DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 
 class EasyAdminSubscriber implements EventSubscriberInterface
 {
-private $slugger;
-private $security;
+    private $slugger;
+    private $security;
+    private $entityManager;
+    private $passwordEncoder;
 
-public function __construct(SluggerInterface $slugger , Security $security)
-{
-$this->slugger = $slugger;
-$this->security = $security;
+    public function __construct(SluggerInterface $slugger, Security $security, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->slugger = $slugger;
+        $this->security = $security;
+        $this->entityManager = $entityManager;
+        $this->passwordEncoder = $passwordHasher;
+    }
 
-}
+    public static function getSubscribedEvents()
+    {
+        return [
+            BeforeEntityPersistedEvent::class => ['setBlogPostSlugAndUser'],
+            BeforeEntityPersistedEvent::class => ['addUser'],
+            BeforeEntityUpdatedEvent::class => ['updateUser'], //surtout utile lors d'un reset de mot passe plutôt qu'un réel update, car l'update va de nouveau encrypter le mot de passe DEJA encrypté ...
 
-public static function getSubscribedEvents()
-{
-return [
-BeforeEntityPersistedEvent::class => ['setBlogPostSlugAndUser'],
-];
-}
+        ];
+    }
 
-public function setBlogPostSlugAndUser(BeforeEntityPersistedEvent $event)
-{
-$entity = $event->getEntityInstance();
+    public function setBlogPostSlugAndUser(BeforeEntityPersistedEvent $event)
+    {
+        $entity = $event->getEntityInstance();
 
-if (!($entity instanceof BlogPost)) {
-return;
-}
+        if (!($entity instanceof BlogPost)) {
+            return;
+        }
 
-$slug = $this->slugger->slugify($entity->getTitle());
-$entity->setSlug($slug);
+        $slug = $this->slugger->slugify($entity->getTitle());
+        $entity->setSlug($slug);
 
-$now = new DateTime('now');
-$entity->setCreatedAt($now);
-$user =$this->security->getUser();
-$entity->setUser($user);
-}
+        $now = new DateTime('now');
+        $entity->setCreatedAt($now);
+        $user = $this->security->getUser();
+        $entity->setUser($user);
+    }
+
+
+    public function updateUser(BeforeEntityUpdatedEvent $event)
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!($entity instanceof Participant)) {
+            return;
+        }
+        $this->setPassword($entity);
+    }
+
+    public function addUser(BeforeEntityPersistedEvent $event)
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!($entity instanceof Participant)) {
+            return;
+        }
+        $this->setPassword($entity);
+    }
+
+    /**
+     * @param User $entity
+     */
+    public function setPassword(Participant $entity): void
+    {
+        $pass = $entity->getPassword();
+
+        $entity->setPassword(
+            $this->passwordEncoder->hashPassword(
+                $entity,
+                $pass
+            )
+        );
+        $this->entityManager->persist($entity);
+        $this->entityManager->flush();
+    }
 }
